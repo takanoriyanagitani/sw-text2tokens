325771a24f37bb86c2ebe3fe23551750423720ba
yanagitani <takanori.yanagitani@gmail.com>
2025-03-12 11:45:04 +0900

    fix: (can be) better build config

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -12,9 +11,14 @@ let package = Package(
     .package(url: "https://github.com/realm/SwiftLint", from: "0.58.2")
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .executableTarget(
-      name: "sw-text2tokens")
+      name: "sw-text2tokens",
+      swiftSettings: [
+        .unsafeFlags(
+          ["-cross-module-optimization"],
+          .when(configuration: .release)
+        )
+      ]
+    )
   ]
 )
